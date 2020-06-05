5d63e7d2d9cd997cb183454cf18acd093ee69e21
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-05 13:26:16 +0100

    Created function that creates array of GifCard components
    In order to populate page with gifs we need a function to extract all the arrays within the state.
    A map is used to parse the array and send each individual gif's url and title to a GifCard component.
    The array of the newly created gifs are then returned in the render statement.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import SearchField from './components/SearchField';
+import React, { Component } from "react";
+import SearchField from "./components/SearchField";
+import GifCard from "./components/GifCard";
 import axios from "axios";
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    gifs: [],
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifs: [],
+    };
+    this.handleSearch = this.handleSearch.bind(this);
   }
-  this.handleSearch = this.handleSearch.bind(this);
-}
-
 
   componentDidMount() {
     this.handleTrendySearch();
@@ -21,38 +20,51 @@ constructor(props) {
   handleTrendySearch = (e) => {
     const url = `http://api.giphy.com/v1/gifs/trending`;
     const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
-    axios.get(url, {params : {api_key: API_KEY}})
+    axios
+      .get(url, { params: { api_key: API_KEY } })
       .then((response) => {
-        const data = response.data;
-        this.setState({gifs: data});
+        const data = response.data.data;
+        this.setState({ gifs: data });
       })
       .catch((err) => {
         console.log(err);
       });
-  }
+  };
 
   handleSearch = (value) => {
     const url = `http://api.giphy.com/v1/gifs/search?q=${value}`;
     const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
-    axios.get(url,{params: {api_key: API_KEY}})
-      .then( response => {
-      const data = response.data;
-      this.setState({gifs: data});
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    axios.get(url, { params: { api_key: API_KEY } })
+      .then((response) => {
+        const data = response.data.data;
+        this.setState({ gifs: data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   render() {
+    const gifs = () => 
+      this.state.gifs.map((gif) => {
+        const url = gif.images.original.webp;
+        const title = gif.title;
+        return (
+        <GifCard
+         url={url}
+         title={title}
+         />
+        );
+      });
     
-  return (
-    <div className="App">
-      <header className="App-header">
-       <SearchField onSubmit={this.handleSearch}/>
-      </header>
-    </div>
-  );
+    return (
+      <div className="App">
+        <header className="App-header">
+          <SearchField onSubmit={this.handleSearch} />
+          <div>{gifs()}</div>
+        </header>
+      </div>
+    );
   }
 }
 
