0a011fa8ec29fbb1cdd2cd3aa20f3c98f3b88143
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-05 10:15:27 +0100

    Added method to make search based on user input
    The render function will create a form with an input text box and a submit button.
     Upon submitting, the form will call the search method which performs a GET request based on the value
     the user has entered in the text field.

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,17 +1,44 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+//import PropTypes from 'prop-types';
+import axios from "axios";
 
 class SearchField extends Component {
-    constructor(props) {
-        super(props);
-        this.state = this.props;
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "dog",
+      gifs: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
-    render() {
-        return <></>
-    }
+  handleSubmit = (e) => {
+      const url = `http://api.giphy.com/v1/gifs/search?q=${this.state.value}`;
+      const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+    axios.get(url,{params: {api_key: API_KEY}})
+      .then( response => {
+        const data = response.data;
+        this.setState({gifs: data});
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+      e.preventDefault();
+  };
+
+  handleChange = (e) => {
+    this.setState({value: e.target.value});
+  }
+
+  render() {
+    return (
+    <form onSubmit={this.handleSubmit}>
+      <input type="text" value={this.state.value} onChange={this.handleChange}/>
+      <input type="submit" value="Submit"/>
+    </form>
+    );
+  }
 }
 
-this.PropTypes = {
-    
-}
+export default SearchField;
