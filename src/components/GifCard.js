4b98ea2cfd4c4cc402134bfffc6ec38e5d1ca68b
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-05 23:41:12 +0100

    Created styles

--- src/components/GifCard.js
+++ src/components/GifCard.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import '../styles/GitCard.css';
 
 class GifCard extends Component {
     render() {
-       return (<img src={this.props.url} alt={this.props.title}/>);
+       return (<img className="gif-img" src={this.props.url} alt={this.props.title}/>);
     }
 }
 export default GifCard;
