0f50c3435938b7f7149563b8699dd338918606f0
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-05 12:09:30 +0100

    Move GET request handler to App.js
    SearchField component has too many methods thus prompting me to break down the component and transfer it's features
    to App.js which will perform the GET requests. SearchField will serve as the input.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,60 @@
-import React from 'react';
-import SearchField from './components/SearchField'
+import React, { Component } from 'react';
+import SearchField from './components/SearchField';
+import axios from "axios";
 import './App.css';
 
-function App() {
+class App extends Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    gifs: [],
+
+  }
+  this.handleSearch = this.handleSearch.bind(this);
+}
+
+
+  componentDidMount() {
+    this.handleTrendySearch();
+  }
+
+  handleTrendySearch = (e) => {
+    const url = `http://api.giphy.com/v1/gifs/trending`;
+    const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+    axios.get(url, {params : {api_key: API_KEY}})
+      .then((response) => {
+        const data = response.data;
+        this.setState({gifs: data});
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  handleSearch = (e) => {
+    const url = `http://api.giphy.com/v1/gifs/search?q=${this.state.value}`;
+    const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
+    axios.get(url,{params: {api_key: API_KEY}})
+      .then( response => {
+      const data = response.data;
+      this.setState({gifs: data});
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+    e.preventDefault();
+  };
+
+  render() {
+
   return (
     <div className="App">
       <header className="App-header">
-       <SearchField/>
+       <SearchField onSubmit={this.handleSearch}/>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
