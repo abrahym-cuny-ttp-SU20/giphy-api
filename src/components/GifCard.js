722ebd3368f47721834ca6063170347ba23a8e0d
Marlon-Urena <45586481+Marlon-Urena@users.noreply.github.com>
2020-06-05 11:03:36 +0100

    Created baseline GifCard component

---
+++ src/components/GifCard.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class GifCard extends Component {
+
+    render() {
+        <img src={this.props.url} alt={this.props.title}/>
+    }
+}
+export default GifCard;
